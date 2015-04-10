b70509a4291fd55ce72a9fee52af6d4f0f840234
Ege Darcin <edarcin@gmail.com>
2015-04-10 12:50:47 +0300

    3rd update for beans
    still having the problem about html, but code seems ok.

--- beans.py
+++ beans.py
@@ -1,10 +1,21 @@
 import urllib.request
-page = urllib.request.urlopen(“http://www.beans-r-us.appspot.com/prices-loyalty.html")
-text = page.read().decode(“utf8")
-where = text.find(">$")
-start_of_price = where + 2
-end_of_price = start of price + 4
-price = tex[start_of_price:end_of_price]
-print(price)
+import time
 
+def get_price
+ page = urllib.request.urlopen(“http://www.beans-r-us.appspot.com/prices-loyalty.html")
+ text = page.read().decode(“utf8")
+ where = text.find(">$")
+ start_of_price = where + 2
+ end_of_price = start of price + 4
+ price = tex[start_of_price:end_of_price]
+ return float(text{start_of_price:end_of_price
 
+price_now = input("Do you want to see the price now(Y/N)?")
+if price_now == "Y"
+   print(get_price())
+else:
+   price = 99.99
+   while price > 4.74:
+    time.sleep(900)
+    price = get_price()
+   print("Buy!")                
