9ff9110a0b6976bcd12e8620e364311434c606d4
Ege Darcin <edarcin@gmail.com>
2015-04-13 18:15:58 +0300

    Results v1.3
    learned hash, sorted it.

--- Results.py
+++ Results.py
@@ -1,14 +1,9 @@
-scores = []
-names = []
+scores = {}
 result_f = open("results.txt")
 for line in result_f:
     (name, score) = line.split()
-    scores.append(float(score))
-    names.append(name)
+    scores[score] = name
 result_f.close()
-scores.sort(reverse = True)
-names.sort (reverse = True)
 print("The Top Scorers Were:")
-print(names[0] + ' scored ' + str(scores[0]))
-print(names[1] + ' scored ' + str(scores[1]))
-print(names[2] + ' scored ' + str(scores[2]))
+for each_score in sorted(scores.keys(), reverse = True):
+    print('Surfer ' + scores[each_score] + ' scored ' + each_score)
