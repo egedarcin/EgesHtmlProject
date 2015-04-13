650407eb1c72707c2ae5e1a513958f0699d93540
Ege Darcin <edarcin@gmail.com>
2015-04-13 10:35:03 +0300

    Beans
    Düzenlendi, çalışıyor.

--- beans.py
+++ beans.py
@@ -1,17 +1,17 @@
 import urllib.request
 import time
 
-def get_price
- page = urllib.request.urlopen(“http://www.beans-r-us.appspot.com/prices-loyalty.html")
- text = page.read().decode(“utf8")
+def get_price():
+ page = urllib.request.urlopen("http://www.beans-r-us.appspot.com/prices-loyalty.html")
+ text = page.read().decode("utf8")
  where = text.find(">$")
  start_of_price = where + 2
- end_of_price = start of price + 4
- price = tex[start_of_price:end_of_price]
- return float(text{start_of_price:end_of_price
+ end_of_price = start_of_price + 4
+ price = (text[start_of_price:end_of_price])
+ return float(text[start_of_price:end_of_price])
 
 price_now = input("Do you want to see the price now(Y/N)?")
-if price_now == "Y"
+if price_now == "Y":
    print(get_price())
 else:
    price = 99.99
