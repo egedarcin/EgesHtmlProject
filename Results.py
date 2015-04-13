234424050914b66f8c6bb817ca95501351d86640
Ege Darcin <edarcin@gmail.com>
2015-04-13 17:52:26 +0300

    Results v1.1
    results devam ediyo.

--- Results.py
+++ Results.py
@@ -1,11 +1,14 @@
 scores = []
+names = []
 result_f = open("results.txt")
 for line in result_f:
     (name, score) = line.split()
     scores.append(float(score))
+    names.append(name)
 result_f.close()
 scores.sort(reverse = True)
+names.sort (reverse = True)
 print("The Top Scorers Were:")
-print(scores[0])
-print(scores[1])
-print(scores[2])
+print(names[0] + ' scored ' + str(scores[0]))
+print(names[1] + ' scored ' + str(scores[1]))
+print(names[2] + ' scored ' + str(scores[2]))
