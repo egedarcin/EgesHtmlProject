390cb690471f53e867aa5eb338e533aac00d38c7
Ege Darcin <edarcin@gmail.com>
2015-05-17 14:10:12 +0300

    309. Sayfaya kadar gelindi.
    tkinter ‘e iyice alışılmaya başlandı

---
+++ Button.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+app = Tk()
+app.title("Button")
+app.geometry('300x100+300+200')
+
+def button_click():
+    print("I've just been touched you pervert!")
+
+b = Button(app, text = "Click on me!", width = 15, command = button_click)
+b.pack(padx = 10, pady = 10)
+
+
+app.mainloop()
