5ff52c50fc488c3e46c634dc6c1832be24ce85c5
Ege Darcin <edarcin@gmail.com>
2015-05-18 12:09:43 +0300

    Message Boxes
    Learned to create message boxes

--- Head-Ex Deliveries.py
+++ Head-Ex Deliveries.py
@@ -1,15 +1,19 @@
 from tkinter import*
+import tkinter.messagebox
 def save_data():
-   fileD = open("deliveries.txt", "a")
-   fileD.write("Depot:\n")
-   fileD.write("%s\n" % depot.get())
-   fileD.write("Description:\n")
-   fileD.write("%s\n" % description.get())
-   fileD.write("Address:\n")
-   fileD.write("%s\n" % address.get("1.0", END))
-   description.delete(0, END)
-   address.delete("1.0", END)
-   depot.set(None)
+   try:
+     fileD = open("deliveries.txt", "a")
+     fileD.write("Depot:\n")
+     fileD.write("%s\n" % depot.get())
+     fileD.write("Description:\n")
+     fileD.write("%s\n" % description.get())
+     fileD.write("Address:\n")
+     fileD.write("%s\n" % address.get("1.0", END))
+     description.delete(0, END)
+     address.delete("1.0", END)
+     depot.set(None)
+   except Exception as ex:
+      tkinter.messagebox.showerror("Error!", "Can't write to the file %s" %ex)
 def read_depots(file):
    depots = []
    depots_f = open(file)
