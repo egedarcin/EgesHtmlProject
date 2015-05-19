8c5ed54e359d6b20e9c396e33e70a8471b45dddd
Ege Darcin <edarcin@gmail.com>
2015-05-19 16:30:24 +0300

    Sayfa 386 ya kadar gelindi, 9.konu bitti

--- DJ Mixer.py
+++ DJ Mixer.py
@@ -3,26 +3,37 @@ import pygame.mixer
 
 app = Tk()
 app.title("Head First Mix")
-app.geometry('250x100+200+100')
 
 sound_file = "Dubstep.wav"
 
 mixer = pygame.mixer
 mixer.init()
 
-def track_start():
-    track.play(loops = -1)
-def track_stop():
-    track.stop()
-
 track = mixer.Sound(sound_file)
 
+def track_toggle():
+    if track_playing.get()==1:
+        track.play(loops = -1)
+    else:
+        track.stop()
+def change_volume(v):
+     track.set_volume(volume.get())
+        
 track_playing = IntVar()
-start_button = Button(app, command = track_start, text = "Start")
-start_button.pack(side = LEFT)
-stop_button = Button(app, command = track_stop, text = "Stop")
-stop_button.pack(side = RIGHT)
-    
+track_button = Checkbutton(app, variable = track_playing, command = track_toggle, text = sound_file)
+track_button.pack(side = LEFT)
+
+volume = DoubleVar()
+volume.set(track.get_volume())
+volume_scale = Scale(variable = volume,
+                     from_ = 0.0,
+                     to = 1.0,
+                     resolution = 0.1,
+                     command = change_volume,
+                     label = "Ses ver",
+                     orient = HORIZONTAL)
+volume_scale.pack(side = RIGHT)
+
 def shutdown():
     mixer.stop()
     app.destroy()
---
+++ Flipper.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+app = Tk()
+app.title("Button")
+app.geometry('300x100+300+200')
+
+flipper = IntVar()
+
+def flip_it():
+    if flipper.get() == 1:
+        print("Cool. I'm all ON, man!")
+    else:
+        print("Phooey. I'm OFF.")
+
+Checkbutton(app, variable = flipper, command = flip_it,text = "Flip it?").pack()
+app.mainloop()
