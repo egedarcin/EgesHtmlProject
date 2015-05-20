4ee8b74bd092461a43b2e98d0b1bda40ff0b92d9
Ege Darcin <edarcin@gmail.com>
2015-05-20 11:26:08 +0300

    quantised the functions in python.

--- DJ Mixer.py
+++ DJ Mixer.py
@@ -1,38 +1,18 @@
 from tkinter import*
+from sound_panel import*
 import pygame.mixer
+
 
 app = Tk()
 app.title("Head First Mix")
 
-sound_file = "Dubstep.wav"
 
 mixer = pygame.mixer
 mixer.init()
 
-track = mixer.Sound(sound_file)
 
-def track_toggle():
-    if track_playing.get()==1:
-        track.play(loops = -1)
-    else:
-        track.stop()
-def change_volume(v):
-     track.set_volume(volume.get())
-        
-track_playing = IntVar()
-track_button = Checkbutton(app, variable = track_playing, command = track_toggle, text = sound_file)
-track_button.pack(side = LEFT)
-
-volume = DoubleVar()
-volume.set(track.get_volume())
-volume_scale = Scale(variable = volume,
-                     from_ = 0.0,
-                     to = 1.0,
-                     resolution = 0.1,
-                     command = change_volume,
-                     label = "Ses ver",
-                     orient = HORIZONTAL)
-volume_scale.pack(side = RIGHT)
+create_gui(app, mixer, "50459_M_RED_Nephlimizer.wav")
+create_gui(app, mixer, "49119_M_RED_HardBouncer.wav")
 
 def shutdown():
     mixer.stop()
